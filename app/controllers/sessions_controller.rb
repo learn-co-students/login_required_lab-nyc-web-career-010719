510801cac35d71d2b23c605cf5f963dc54e67702
Your Name <github email address>
2019-02-11 19:20:36 -0500

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,7 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
   def create
     return redirect_to(controller: 'sessions', action: 'new') if !params[:name] || params[:name].empty?
     session[:name] = params[:name]
@@ -9,4 +12,5 @@ class SessionsController < ApplicationController
     session.delete :name
     redirect_to controller: 'application', action: 'hello'
   end
+  
 end
